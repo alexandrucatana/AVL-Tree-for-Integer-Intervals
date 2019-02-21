a935431b18701b0915d7cfb0df60f07d19829efd
alex <alex@u-080-c180.eap.uni-tuebingen.de>
2019-02-21 20:11:07 +0100

    Doxygen Documentation changed

--- docs/html/search/functions_5.js
+++ docs/html/search/functions_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['getroot',['getRoot',['../classAVL.html#ae4d224d8a9f8ae335f8451ef47cbb026',1,'AVL']]]
+  ['getroot',['getRoot',['../classAVL.html#ae4d224d8a9f8ae335f8451ef47cbb026',1,'AVL']]],
+  ['getsortedlistfromavl',['getSortedListFromAvl',['../classAVL.html#a85e62f7a85a8c491d2297653056a13a4',1,'AVL']]]
 ];
